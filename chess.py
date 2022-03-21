36ffd0d7049e3001be7eddd41c96d8aa73b76fb8
stephen911 <stephendappah1@gmail.com>
2022-03-21 00:41:18 +0000

    'new'

--- chess.py
+++ chess.py
@@ -128,3 +128,5 @@
 ~ give it more variety in terms of the move it plays. Furthermore, using good openings
 ~ allows the AI to make the best moves in the field it is best at: middle game tactics.
 ~ 5. Better evaluation of positions - The current features evaluated by the evaluation
+~ function when judging a positoin to give it a score allows for good opening games and
+~ tactics that often allow it to gain advantage over the opponents that I have tested it
