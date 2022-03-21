17270ce52c8b3444830704d39251893b9f283d30
stephen911 <stephendappah1@gmail.com>
2022-03-21 00:49:54 +0000

    'new'

--- chess.py
+++ chess.py
@@ -192,4 +192,5 @@ class GamePosition:
                                                                                     ~detect draw by 50 moves without any capture or pawn movement.
                                                                                             self.history = history ~A dictionary that stores as key a position (hashed) and the value of each of
                                                                                                     ~these keys represents the number of times each of these positions was repeated in order for this
-                                                                                                    )
+                                                                                                            ~position to be reached.
+                                                                                                            )
